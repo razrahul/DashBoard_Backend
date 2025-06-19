5bee3b1e29d8348a8cad4d4a4df373d763c14534
Rahul Kumar <158333668+razrahul@users.noreply.github.com>
2025-06-19 22:57:41 +0530

    Feature/emailotp (#9)
    * phone otp complete
    
    * email  otp add
    
    ---------
    
    Co-authored-by: razrahul <razrahuleee.com>

--- src/utils/commonMethod.js
+++ src/utils/commonMethod.js
@@ -1,25 +1,3 @@
-// const Roles =  require("../models/role");
-// const Users = require("../models/user");
-// const Companys = require("../models/company");
-// const Chats = require("../models/chat");
-// const Contactc = require("../models/contact");
-// const ContactLogs = require("../models/contactLog");
-// const Stats = require("../models/stats");
-
-// const tableSync = async () => {
-//   try {
-//     await Roles.sync({ force: false });
-//     await Companys.sync({ force: false }); 
-//     await Users.sync({ force: false });
-//     await Chats.sync({ force: false });
-//     await Contactc.sync({ force: false });
-//     await ContactLogs.sync({ force: false });
-//     await Stats.sync({ force: false });
-//   } catch (error) {
-//     throw new Error(error.message);
-//   }
-// };
-
 const Auth = require("../models/auth");
 
 const tableSync = async () => {
@@ -34,6 +12,3 @@ const tableSync = async () => {
 
 module.exports = { tableSync };
 
-// await Chats.sync({ alter: true }); 
-
-// wait Chats.sync({ force: true });
